8d19028955d220ab42beafd49626d2982370a996
Phil Curzon <phil@novelfs.org>
2023-06-27 00:16:11 +0100

    marking tightness based on distance from ball

--- src/Football/Behaviours/Marking.hs
+++ src/Football/Behaviours/Marking.hs
@@ -23,21 +23,39 @@ import Football.Understanding.Shape (outOfPossessionDesiredPosition)
 import Football.Types
 import Football.Behaviours.Marking.Zonal (mostDangerousPlayerInZone)
 import Football.Understanding.Zones.Types (ZoneCache)
-import Football.Understanding.Team (inTeamCoordinateSystem, toTeamCoordinateSystem)
+import Football.Understanding.Team (inTeamCoordinateSystem, toTeamCoordinateSystem, fromTeamCoordinateSystem)
 
 playerMarkClosestOppositionPlayer :: (Monad m, Match m, Log m, Cache m CentresOfPlayCache) => Player -> m (V2 Double)
 playerMarkClosestOppositionPlayer player = do
   oppositionPlayers' <- oppositionPlayers (playerTeam player)
   teamPlayers' <- teamPlayers (playerTeam player)
+  ball <- gameBall
   let matchUp = foldl' (folder teamPlayers') Map.empty oppositionPlayers'
   case Map.lookup player matchUp of
-    Just p | playerNumber player /= 1 -> locate2D <$> inTeamCoordinateSystem (playerTeam player) (playerPositionVector p) (+ V3 (-2) 0 0)
+    Just p | playerNumber player /= 1 -> do
+      tBall <- toTeamCoordinateSystem (playerTeam player) (ballPositionVector ball)
+      tTargetPlayer <- toTeamCoordinateSystem (playerTeam player) (playerPositionVector p)
+      let tDiff = tBall - tTargetPlayer
+      let desiredDist x = 1.8 * exp ( 0.075 * x )
+      let desiredDistTP = desiredDist (norm tDiff)
+      let minimumPos = tTargetPlayer - pure desiredDistTP * V3 1 0 0
+      let maximumPos = tTargetPlayer - V3 1.8 0 0
+      formationPos <- outOfPossessionDesiredPosition player
+      let ultimatePos = resolvePosition minimumPos maximumPos formationPos
+      locate2D <$> fromTeamCoordinateSystem (playerTeam player) ultimatePos
+      --locate2D <$> inTeamCoordinateSystem (playerTeam player) (playerPositionVector p) (+ V3 (-2) 0 0)
     _ -> outOfPossessionDesiredPosition player
   where 
     folder teamPlayers' acc p =
       case find (\p' -> isNothing $ Map.lookup p' acc) $ sortOn (distance (playerPositionVector p) . playerPositionVector) teamPlayers' of
         Just teamP -> Map.insert teamP p acc
         Nothing -> acc
+    resolvePosition minPos maxPos formPos =
+      let x = max (minPos ^. _x) $ min (maxPos ^. _x) (formPos ^. _x)
+          y = maxPos ^. _y
+      in V3 x y 0 
+
+
 
 positionalOrientedZonalMark :: (Monad m, Match m, Log m, Cache m CentresOfPlayCache) => Player -> m (V2 Double)
 positionalOrientedZonalMark player = do
